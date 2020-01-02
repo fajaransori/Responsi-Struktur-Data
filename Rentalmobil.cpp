fdf25f64c0607dd634ddf0d723eb5eeb2cbf800f
HilmiAziz_5170411169 <43813159+hilmiaziz@users.noreply.github.com>
2020-01-02 22:32:07 +0700

    membuat file rental mobil.cpp
    menambahkan deklarasi main

---
+++ Rentalmobil.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <math.h>
+
+int main(){
+
+	Queue Driver;
+	Konsumen konsumen;
+	List mobil;
+	Transaksi Penyewaan;
+	pembayaran Bayar_sewa;
+
+	address data;
+	ID_Transaksi Data_Pinjam;
+	ID_Pembayaran Data_Pembayaran;
+
+
+	infotype tnkb,merk,warna,tahun;
+	int biayasewa = 0;
+	int denda = 0;
+	int totbiaya = 0;
+	infotype nama,ktp,alamat,umur,nama_driver;
+	int no=0;
+	int pilih;
+	int select;
+	char opsi;
+	Date sewa;
+	Date kembali;
+	Date bayar;
+  }
